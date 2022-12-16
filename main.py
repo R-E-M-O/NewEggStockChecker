82aa3b19d8871981959ff2c26f0772cad521c158
Omer H <73497880+R-E-M-O@users.noreply.github.com>
2022-12-15 19:34:44 -0500

    Initial commit with prior implementations

---
+++ main.py
@@ -0,0 +1,94 @@
+import requests
+import time
+import random
+import threading
+from datetime import datetime
+
+numThreads = 2
+
+# Nintendo switch, PS5, Xbox Series S, MSI GAMING PC, Corsair Gaming PC, ROG Gaming PC
+urls = [
+    "https://www.newegg.com/white-hegskaaaa-nintendo-switch-console/p/N82E16878190943",
+    "https://www.newegg.com/p/N82E16868110298",
+    "https://www.newegg.com/p/N82E16868105280",
+    "https://www.newegg.com/msi-mpg-trident-as-12td-030us/p/N82E16883152973",
+    "https://www.newegg.com/abs-ali646/p/N82E16883360311",
+    "https://www.newegg.com/asus-ga15dk-ib778-rog-strix-ga15/p/N82E16883221709"
+]
+
+# array to store each product log
+logs = ["", "", "", "", "", ""]
+
+# handles http get requests, uses mutex lock to isolate the critical section
+def checkNeweggStock(url, lock):
+    lock.acquire()
+    r = requests.get(url, timeout=5)
+    lock.release()
+    # string holding all HTML code
+    return str(r.content)
+
+
+def newEgg(url, lock):
+    # HTML Response Data
+    htmlResponse = checkNeweggStock(url, lock)
+
+    # status of labels
+    inStock = htmlResponse[htmlResponse.index('"Instock"') +
+                           10:htmlResponse.index('"Stock"') - 1]
+    price = htmlResponse[htmlResponse.index('"FinalPrice":') + 13:htmlResponse.index('"Instock"') - 1]
+    shipping = htmlResponse[htmlResponse.index('"ShippingCharge"') + 17:htmlResponse.index('"VFAvail"') - 1]
+    quantity = int(htmlResponse[htmlResponse.index('"Stock":') + 8:htmlResponse.index('"StockForCombo"') - 1])
+    gpuName = htmlResponse[htmlResponse.index('<title>') + 7:htmlResponse.index('</title>') - 13]
+
+    # timezone formatting
+    utcTime = datetime.utcnow()
+    formattedEDT = utcTime.strftime('%Y-%m-%d %H:%M:%S')
+
+    # combine the parsed string fragments together
+    inStockLog = formattedEDT + " NEWEGG - " + gpuName + ' | Price: $' + str(price) + ' Shipping Cost: $' + str(
+            shipping) + ' Quantity: ' + str(quantity)
+
+
+    if inStock == 'false':
+        logs[urls.index(url)] = formattedEDT + " NEWEGG - " + gpuName + "OUT OF STOCK"
+    else:
+        logs[urls.index(url)] = inStockLog
+
+# prints each stock log
+def printLogs(log):
+    print(log)
+
+while True:
+    # any error that may occur will either be caused by spam detection or website traffic/outage
+    try:
+        # initialize mutex lock
+        lock = threading.Lock()
+        # executes item stock-checking in parallel for each url in the url array
+        for i in range(len(urls)):
+            thread = threading.Thread(target=newEgg, args=(urls[i], lock,))
+            thread.start()
+
+        # joins threads together when done. newEgg() will not run anymore in this iteration
+        for i in range(len(urls)):
+            thread.join()
+
+        # previous join forces program to wait for each thread to finish
+        # now the threads are utilized to print messages to console in parallel
+        for i in range(len(urls)):
+            thread = threading.Thread(target=printLogs, args=(logs[i], ))
+            thread.start()
+
+        # ensures program will not advance until all item stock messages are printed, joins threads together
+        for i in range(len(urls)):
+            thread.join()
+    except:
+        # handles any error that may occur as stated above, and quits the program accordingly.
+        print('Detected by captcha! Exiting program.')
+        quit()
+
+    # new line for tidiness
+    print()
+
+    # program waits a random amount of time between 3.0 to 6.0 seconds
+    # theoretically, less likely to activate website captcha that would break the program
+    time.sleep(random.uniform(3, 6))
---
+++ mainSequential.py
@@ -0,0 +1,60 @@
+import requests
+import time
+import random
+from datetime import datetime
+
+# Nintendo switch, PS5, Xbox Series S, MSI GAMING PC, Corsair Gaming PC, ROG Gaming PC
+urls = [
+    "https://www.newegg.com/white-hegskaaaa-nintendo-switch-console/p/N82E16878190943",
+    "https://www.newegg.com/p/N82E16868110298",
+    "https://www.newegg.com/p/N82E16868105280",
+    "https://www.newegg.com/msi-mpg-trident-as-12td-030us/p/N82E16883152973",
+    "https://www.newegg.com/abs-ali646/p/N82E16883360311",
+    "https://www.newegg.com/asus-ga15dk-ib778-rog-strix-ga15/p/N82E16883221709"
+]
+
+# handles http get requests
+def checkNeweggStock(url):
+    r = requests.get(url, timeout=5)
+    # string holding all HTML code
+    return str(r.content)
+
+
+def newEgg(url):
+    # HTML Response Data
+    htmlResponse = checkNeweggStock(url)
+
+    # status of labels
+    inStock = htmlResponse[htmlResponse.index('"Instock"') +
+                           10:htmlResponse.index('"Stock"') - 1]
+    price = htmlResponse[htmlResponse.index('"FinalPrice":') + 13:htmlResponse.index('"Instock"') - 1]
+    shipping = htmlResponse[htmlResponse.index('"ShippingCharge"') + 17:htmlResponse.index('"VFAvail"') - 1]
+    quantity = int(htmlResponse[htmlResponse.index('"Stock":') + 8:htmlResponse.index('"StockForCombo"') - 1])
+    gpuName = htmlResponse[htmlResponse.index('<title>') + 7:htmlResponse.index('</title>') - 13]
+
+    # timezone formatting
+    utcTime = datetime.utcnow()
+    formattedEDT = utcTime.strftime('%Y-%m-%d %H:%M:%S')
+
+    for i in range(len(urls)):
+        inStockLog = formattedEDT + " NEWEGG - " + gpuName + ' | Price: $' + str(price) + ' Shipping Cost: $' + str(
+            shipping) + ' Quantity: ' + str(quantity)
+
+    if inStock == 'false':
+        print(formattedEDT + " NEWEGG - " + gpuName + "OUT OF STOCK")
+    else:
+        print(inStockLog)
+
+def printLogs(log):
+    print(log)
+
+while True:
+    try:
+        for i in range(len(urls)):
+            newEgg(urls[i])
+    except:
+        print('Detected by captcha! Exiting program.')
+        quit()
+
+    print()
+    time.sleep(random.uniform(3, 6))
